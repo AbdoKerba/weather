658990de0081ee378295b47d0c9251f111512c5e
AbdoKerba <abdokerba33@gmail.com>
2023-08-26 02:24:30 +0300

    ''

--- detector/views.py
+++ detector/views.py
@@ -8,14 +8,14 @@ def index(request):
         city = request.POST['city']
         req = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=594708214e839c9292443ce74ab32272").read()
         json_data = json.loads(req)
-        if json_data['cod'] == '404':
+        if str(json_data['cod']) == '404':
             return HttpResponse("Invalid city")
         data = {
             'country': json_data['sys']['country'],
             'temp': round(json_data['main']['temp'] - 272.15),
             'description': json_data['weather'][0]['description'],
             'pressure': json_data['main']['pressure'],
-            'wind': json_data['cod'],
+            'wind': json_data['wind']['speed'],
             'city': city
         }
     else:
