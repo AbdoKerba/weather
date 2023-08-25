3715442b987866203c828c3c650c74cf2053cae0
AbdoKerba <abdokerba33@gmail.com>
2023-08-26 02:15:57 +0300

    ''

--- detector/views.py
+++ detector/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 import json
 import urllib.request
 # Create your views here.
@@ -7,17 +7,17 @@ def index(request):
     if request.method == 'POST':
         city = request.POST['city']
         req = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=594708214e839c9292443ce74ab32272").read()
-        if req:
-            json_data = json.loads(req)
-            data = {
-                'country': json_data['sys']['country'],
-                'temp': round(json_data['main']['temp'] - 272.15),
-                'description': json_data['weather'][0]['description'],
-                'pressure': json_data['main']['pressure'],
-                'wind': json_data['wind']['speed'],
-                'city': city}
-        else:
-            data = {}
+        json_data = json.loads(req)
+        if json_data['cod'] == '404':
+            return HttpResponse("Invalid city")
+        data = {
+            'country': json_data['sys']['country'],
+            'temp': round(json_data['main']['temp'] - 272.15),
+            'description': json_data['weather'][0]['description'],
+            'pressure': json_data['main']['pressure'],
+            'wind': json_data['wind']['speed'],
+            'city': city
+        }
     else:
         data = {}
     return render(request, 'index.html', data)
